0476fd05e9786a322434161986ce240ee58b1fb7
Archit Jain <architjain798@gmail.com>
2024-01-14 16:32:26 +0530

    Add the custom createelement

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-    xyz: "abc",
-  },
-  "Hello World"
-);
+// ReactElement(Object) => HTML(Browser Understands)
 
-console.log(heading); //object
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child-1" }, [
+    React.createElement("h1", {}, "H1 tag of child-1"),
+    React.createElement("h2", {}, "H2 tag of child-1"),
+  ]),
+  React.createElement("div", { id: "child-2" }, [
+    React.createElement("h1", {}, "H1 tag of child-2"),
+    React.createElement("h2", {}, "H2 tag of child-2"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//     xyz: "abc",
+//   },
+//   "Hello World"
+// );
+
+// console.log(heading); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
